407867fa2e615b234c1a0e69e6fe6853b8d3dc34
Bsetecip10 <bsetecip10@Nirmals-Mac-mini.local>
2014-12-02 11:27:40 +0530

    Initial Commit

---
+++ ThreadUsingDespatchQue/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ThreadUsingDespatchQue
+//
+//  Created by Bsetecip10 on 02/12/14.
+//  Copyright (c) 2014 gyana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ThreadUsingDespatchQue/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ThreadUsingDespatchQue
+//
+//  Created by Bsetecip10 on 02/12/14.
+//  Copyright (c) 2014 gyana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
